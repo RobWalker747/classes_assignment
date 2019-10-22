cdbca13d93cd48adfd3bffd0819689982e2220ef
trilltown <phil.awich@gmail.com>
2019-10-21 21:45:27 -0400

    Solve problems 3abc

--- class.js
+++ class.js
@@ -27,25 +27,60 @@
 
 // a. Create a class called `Book` that has properties `title`, `author` and `rating`. Create some instances of `Book`.
 
-class Book {
-    constructor(title, author, rating) {
-        this.title = title;
-        this.author = author; 
-        this.rating = rating;
-    }
-    isGood() {
-        if (this.rating >= 7) {
-            return true
+// class Book {
+//     constructor(title, author, rating) {
+//         this.title = title;
+//         this.author = author; 
+//         this.rating = rating;
+//     }
+//     isGood() {
+//         if (this.rating >= 7) {
+//             return true
 
-        }
-    }
-}
+//         }
+//     }
+// }
 
-let novela = new Book("Becoming Michelle Obama", "Michelle Obama", 10)
-let novel = new Book("Shoe Dog", "Phil Knight", 9)
-let nove = new Book("The Fault in Our Stars", "John Green", 6)
-let no = new Book("True Blood", "Charlaine Harris", 5)
+// let novela = new Book("Becoming Michelle Obama", "Michelle Obama", 10)
+// let novel = new Book("Shoe Dog", "Phil Knight", 9)
+// let nove = new Book("The Fault in Our Stars", "John Green", 6)
+// let no = new Book("True Blood", "Charlaine Harris", 5)
 
 // b. Add a method to `Book` called `isGood` that returns `true` if its rating is greater than or equal to 7
 
-console.log(novela.isGood());
+// console.log(novela.isGood());
+
+// ## Question 3
+
+// a. Create a `Dog` class with four properties: `name (string), breed (string), mood (string), and hungry (boolean)`.
+
+class Dog {
+    constructor(name, breed, mood, hungry) {
+        this.name = name;
+        this.breed = breed;
+        this.mood = mood;
+        this.hungry = hungry;
+    }
+    playFetch()  {
+        this.hungry === false && this.mood === 'playful'
+        return 'Ruff!'
+        }
+        feed() {
+            if (this.hungry === false) {
+                console.log("Woof!")
+            } else {
+                console.log("The dog doesn't look hungry")
+            }
+            toString() {
+                return this.mood 
+            }
+        }
+    }
+
+    let wolf = new Dog("Roxy", "Pitbull", "Loveable" , true)
+    console.log(wolf.feed());
+    // b. Add a method called `playFetch`. It should set the dog's `hungry` property to `true`, set its mood property to `playful`, and log "Ruff!"
+
+// c. Add a method called `feed`. If the dog is hungry, it should set `hungry` to `false` and print "Woof!" If the dog is not hungry, it should log "The dog doesn't look hungry"
+
+// d. Add a method called `toString` that returns a description of the dog:
